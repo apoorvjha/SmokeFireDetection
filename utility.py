39786034dd7720b9ac9bfa1fc630a7051d1cd29f
Apoorv <apoorvjha18@gmail.com>
2021-10-31 23:15:06 +0530

    Added code for iamge preprocessing and debugging.

--- utility.py
+++ utility.py
@@ -3,8 +3,8 @@ from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPool2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.metrics  import AUC, CategoricalAccuracy, FalsePositives
-from cv2 import imread, VideoCapture
-from numpy import array
+from cv2 import imread, VideoCapture, GaussianBlur, cvtColor, INTER_CUBIC,COLOR_BGR2GRAY, resize, imshow, waitKey, destroyAllWindows
+from numpy import array, max
 from os.path import exists
 from os import listdir
 from datetime import datetime
@@ -20,23 +20,13 @@ class Logger:
 
 
 log=Logger()
-'''
-============================================= Think arena ======================================================== 
-https://towardsdatascience.com/comparative-performance-of-deep-learning-optimization-algorithms-using-numpy-24ce25c2f5e2
 
-smoke = [1,0]
-fire = [0,1]
-smoke + fire = [1,1]
-
-=> unsafe if Prediction_vector[0] OR Prediction_vector[1]
-==================================================================================================================
-'''
 class Model:
     def __init__(self,input_shape,stride=(1,1),dilation=(1,1),kernel_n=3,pooling_size=(2,2),dropout_p=0.2,n_output=2,learning_rate=1e-3):
         log.log("[+] Model Initilaizing...")
         self.kernel_n=kernel_n
         self.input_shape=input_shape 
-        self.stride=stride      # skips kernel makes at every convolution
+        self.stride=stride      # skips, kernel makes at every convolution
         self.dilation=dilation  # kernel coverage
         self.pooling_size=pooling_size
         self.dropout_p=dropout_p
@@ -98,15 +88,16 @@ class Model:
         log.log("----------------------------------------------------------------")
 
 class Data:
-    def __init__(self):
+    def __init__(self,widthX=128,widthY=128):
         self.data_folder="./static/dataset/"
+        self.dimension=(widthX,widthY)
     def get_data_dataset(self,mode=0,color_mode=0):
         # mode=0 ; Image data will be returned.
         # mode=1 ; Video data will be returned.
         # color_mode=0 ; greyscale image/frame.
         # color_mode=1 ; colour channel image/frame.
         # color_mode=-1 ; color channel along with alpha channel image/frame.
-        if mode==1:
+        if mode==0:
             X=[]
             Y=[]
             directory=self.data_folder+"images/FIRE_SMOKE/"
@@ -117,7 +108,7 @@ class Data:
             for i in listdir(directory):
                 X.append(imread(directory+i,color_mode))
                 Y.append(0)
-            return array(X),array(Y)
+            return X,Y
         else:
             X=[]
             Y=[]
@@ -127,7 +118,7 @@ class Data:
                 while(cap.isOpened()):
                     ret,frame=cap.read()
                     if ret==True:
-                        X.append(imread(frame,color_mode))
+                        X.append(frame,color_mode)
                         Y.append(1)
             directory=self.data_folder+"videos/NONE/"
             for i in listdir(directory):
@@ -135,15 +126,15 @@ class Data:
                 while(cap.isOpened()):
                     ret,frame=cap.read()
                     if ret==True:
-                        X.append(imread(frame,color_mode))
+                        X.append(frame)
                         Y.append(0)
-            return array(X),array(Y)
+            return X,Y
     def read_data(self,directory,mode=0,color_mode=0):
         if mode==0:
             X=[]
             for i in listdir(directory):
                 X.append(imread(directory+i,color_mode))
-            return array(X)
+            return X
         else:
             X=[]
             for i in listdir(directory):
@@ -151,17 +142,28 @@ class Data:
                 while(cap.isOpened()):
                     ret,frame=cap.read()
                     if ret==True:
-                        X.append(imread(frame,color_mode))
-            return array(X)
-    def preprocessing(self):
+                        X.append(frame)
+            return X
+    def preprocessing(self,image):
+        # Noise removal using gaussian smoothing operator.
+        image=GaussianBlur(image,(3,3),0)
+        image=resize(image,self.dimension,interpolation=INTER_CUBIC)
+        return image
+    def normalize(self,image):
+        return image/255
+    def show_image(self,image,window_name):
+        imshow(window_name, image)
+        waitKey(0) 
+        destroyAllWindows()
+
+
             
 
 
 
-        
-
-        
-
-
-model=Model(input_shape=(10,28,28,3))
-model.getStats()
+data=Data()
+images,classes=data.get_data_dataset(color_mode=1)
+data.show_image(images[0], "RAW")
+print(max(images[0])) 
+image=data.preprocessing(images[0])
+data.show_image(image, "PEOCESSED")
